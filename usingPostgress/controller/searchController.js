1d76971c3ef99dcffe09bbbb50f51ea3e8b0e9a2
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-04-04 19:50:49 +0530

    added fuzzy search in combination with full-text-search

--- usingPostgress/controller/searchController.js
+++ usingPostgress/controller/searchController.js
@@ -54,3 +54,42 @@ export const dynamicSearch = async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 }
+
+export const combinedSearch = async (req, res) => {
+  const { column, term } = req.query; // Get search column and term from query parameters
+  try {
+    let query
+    const searchWords = term.split(' ').filter(word => word.length > 0)
+    const searchValues = searchWords.map(word => `${word}`).join(' & ')
+    if (column && term) {
+      query = `SELECT * FROM actionmovies WHERE
+      ${column}_search @@ to_tsquery('english', $1)
+      OR similarity(${column}, $1) > 0.3`;
+
+    }
+    else if (term) {
+      query = `
+      SELECT *
+      FROM actionmovies
+      WHERE 
+      movie_name_search @@ to_tsquery('english', $1)
+      OR star_search @@ to_tsquery('english', $1)
+      OR director_search @@ to_tsquery('english', $1)
+      OR genre_search @@ to_tsquery('english', $1)
+      OR similarity(movie_name, $1) > 0.3
+      OR similarity(star, $1) > 0.3
+      OR similarity(director, $1) > 0.3
+      OR similarity(genre, $1) > 0.3
+       `;
+    }
+    else {
+      return res.status(400).json({ error: 'Missing required parameters' });
+    }
+    const result = await pool.query(query, [searchValues]);
+
+    res.status(200).json({ total_results: result.rowCount, movies: result.rows });
+  } catch (error) {
+    console.error('Error executing search query:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
--- usingPostgress/routes/searchRoutes.js
+++ usingPostgress/routes/searchRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { searchSimple ,dynamicSearch} from "../controller/searchController.js";
+import { searchSimple ,dynamicSearch, combinedSearch} from "../controller/searchController.js";
 
 const searchRouter =express()
 
 searchRouter.get('/search',searchSimple)
 searchRouter.get('/dynamicsearch',dynamicSearch)
+searchRouter.get('/combinedsearch',combinedSearch)
 
 export default searchRouter
